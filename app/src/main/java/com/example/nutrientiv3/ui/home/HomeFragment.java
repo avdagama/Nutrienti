1260cdca0315ea98f71372e136485432431e88ee
Alex DaGama <avdagama@Alexs-MBP.lan>
2021-02-20 18:26:47 -0500

    main circle

--- app/src/main/java/com/example/nutrientiv3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/nutrientiv3/ui/home/HomeFragment.java
@@ -8,11 +8,18 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.nutrientiv3.MainActivity;
 import com.example.nutrientiv3.R;
+import com.txusballesteros.widgets.FitChart;
+import com.txusballesteros.widgets.FitChartValue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -23,13 +30,28 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        //final TextView textView = root.findViewById(R.id.text_home);
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //    @Override
+        //    public void onChanged(@Nullable String s) {
+        //        textView.setText(s);
+        //    }
+        //});
+
+        // FIT CHART
+        FitChart fitChart = root.findViewById(R.id.fitChart);
+        fitChart.setMinValue(0f);
+        fitChart.setMaxValue(100f);
+
+        // Create a list with the set values
+        List<FitChartValue> values = new ArrayList<>();
+        values.add(new FitChartValue(50f, ContextCompat.getColor(getContext(), R.color.chart_value_1)));
+        values.add(new FitChartValue(25f, ContextCompat.getColor(getContext(), R.color.chart_value_2)));
+        values.add(new FitChartValue(50f, ContextCompat.getColor(getContext(), R.color.chart_value_3)));
+
+        // Add the Collection to the wheel chart
+        fitChart.setValues(values);
+
         return root;
     }
 }
--- app/src/main/java/com/example/nutrientiv3/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/nutrientiv3/ui/home/HomeViewModel.java
@@ -1,8 +1,16 @@
 package com.example.nutrientiv3.ui.home;
 
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.nutrientiv3.R;
+import com.txusballesteros.widgets.FitChart;
+import com.txusballesteros.widgets.FitChartValue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
 
@@ -11,6 +19,7 @@ public class HomeViewModel extends ViewModel {
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+
     }
 
     public LiveData<String> getText() {
