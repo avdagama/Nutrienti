bf1bb26c8fd9652cf016f27b3b04bb58cc0aec7c
Alex DaGama <avdagama@hotmail.com>
2021-02-25 18:17:40 -0500

    auth theme

--- app/src/main/java/com/example/nutrientiv3/MainActivity.java
+++ app/src/main/java/com/example/nutrientiv3/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.nutrientiv3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUserMetadata;
 import com.txusballesteros.widgets.FitChart;
 import com.txusballesteros.widgets.FitChartValue;
 
@@ -23,6 +28,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +46,25 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
         getSupportActionBar().hide();
 
-
+        //Firebase Authentication
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
-            // already signed in
+            FirebaseUserMetadata metadata = auth.getCurrentUser().getMetadata();
+            if (metadata.getCreationTimestamp() == metadata.getLastSignInTimestamp()) {
+                // The user is new, show them a fancy intro screen!
+            } else {
+                // This is an existing user, show them a welcome back screen.
+                System.out.println(auth.getUid());
+            }
         } else {
             // not signed in
-            int RC_SIGN_IN = 123;
+
             // Get an instance of AuthUI based on the default app
             startActivityForResult(
                     AuthUI.getInstance()
                             .createSignInIntentBuilder()
+                            .setTheme(R.style.GreenTheme)
+                            .setLogo(R.drawable.logo)
                             .setAvailableProviders(Arrays.asList(
                                     new AuthUI.IdpConfig.GoogleBuilder().build(),
                                     new AuthUI.IdpConfig.EmailBuilder().build(),
@@ -58,10 +72,33 @@ public class MainActivity extends AppCompatActivity {
                                     new AuthUI.IdpConfig.AnonymousBuilder().build()))
                             .build(),
                     RC_SIGN_IN);
+
         }
-
-
-
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    return;
+                }
+
+                Log.e("TAG", "Sign-in error: ", response.getError());
+            }
+        }
+    }
 }
